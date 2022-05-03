dc9476f7b29ae5f3a9caf8342951f633cd712617
vineethsudhir <vineeth.sudhir@gmail.com>
2022-05-03 13:46:39 -0400

    Tests added

---
+++ tests/cred_test.py
@@ -0,0 +1,79 @@
+"""This tests login , registration and dashboard authentication"""
+from flask import url_for
+
+
+def test_register(client):
+    """This tests for successful registration"""
+    with client:
+        register_response = client.post("/register", data={
+            "email": "testuser1@test.com",
+            "password": "test123!test",
+            "confirm": "test123!test"
+        },
+                                        follow_redirects=True)
+        # After successful registration,redirected to login page
+        assert register_response.status_code == 200
+        assert register_response.request.path == url_for('auth.login')
+
+
+def test_login(client):
+    """This tests for successful login"""
+    with client:
+        register_response = client.post("/register", data={
+            "email": "testuser1@test.com",
+            "password": "test123!test",
+            "confirm": "test123!test"
+        },
+                                        follow_redirects=True)
+
+        assert register_response.status_code == 200
+        assert register_response.request.path == url_for('auth.login')
+
+        login_response = client.post("/login", data={
+            "email": "testuser1@test.com",
+            "password": "test123!test"
+        },
+                                     follow_redirects=True)
+        # After successful login ,redirected to dashboard
+        assert login_response.request.path == url_for('auth.dashboard')
+        assert login_response.status_code == 200
+
+
+def test_dashboard_access(client):
+    """This tests for successful access to dashboard after login"""
+    with client:
+        register_response = client.post("/register", data={
+            "email": "testuser1@test.com",
+            "password": "test123!test",
+            "confirm": "test123!test"
+        },
+                                        follow_redirects=True)
+
+        login_response = client.post("/login", data={
+            "email": "testuser1@test.com",
+            "password": "test123!test"
+        }, follow_redirects=True)
+        assert login_response.request.path == url_for('auth.dashboard')
+        assert login_response.status_code == 200
+
+
+def test_dashboard_access_denied(client):
+    """This tests for unsuccessful access to dashboard after login"""
+    with client:
+        register_response = client.post("/register", data={
+            "email": "testuser1@test.com",
+            "password": "test123!test",
+            "confirm": "test123!test"
+        },
+                                        follow_redirects=True)
+
+        assert register_response.status_code == 200
+        assert register_response.request.path == url_for('auth.login')
+
+        login_response = client.post("/login", data={
+            "email": "testuser1@test.com",
+            "password": "test1234!test"
+        },
+                                     follow_redirects=True)
+        assert login_response.request.path == url_for('auth.login')
+        assert login_response.status_code == 200
--- tests/simple_pages_test.py
+++ tests/simple_pages_test.py
@@ -1,7 +1,8 @@
 """This test the homepage"""
 
+
 def test_request_main_menu_links(client):
-    """This makes the index page"""
+    """This tests the index page"""
     response = client.get("/")
     assert response.status_code == 200
     assert b'href="/about"' in response.data
@@ -11,24 +12,27 @@ def test_request_main_menu_links(client):
 
 
 def test_request_index(client):
-    """This makes the index page"""
+    """This tests the index page"""
     response = client.get("/")
     assert response.status_code == 200
     assert b"Index" in response.data
 
+
 def test_request_about(client):
-    """This makes the index page"""
+    """This tests the about page"""
     response = client.get("/about")
     assert response.status_code == 200
     assert b"About" in response.data
 
+
 def test_request_page1(client):
-    """This makes the index page"""
+    """This tests the welcome page"""
     response = client.get("/welcome")
     assert response.status_code == 200
     assert b"welcome" in response.data
 
+
 def test_request_page_not_found(client):
-    """This makes the index page"""
+    """This tests is for the page not found error"""
     response = client.get("/page5")
     assert response.status_code == 404
